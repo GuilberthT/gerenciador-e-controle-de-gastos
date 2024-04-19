9fc0cc40a868450149e8d70cceef2bb9c4324d07
Guilberth12 <Guilbertw@gmail.com>
2024-04-19 16:38:39 -0400

    validações de types

--- src/controller/type-expenses/createExpenseType.js
+++ src/controller/type-expenses/createExpenseType.js
@@ -2,8 +2,19 @@ import { createExpenseType } from "../../service/expenseTypeService.js";
 
 export async function create_expense_type_controller(req, res) {
     try {
+        await validates(req.body);
         await createExpenseType(req, res);
     } catch (error) {
         res.status(500).json({ message: "Erro interno do servidor" });
+    }
+}
+
+async function validates(body) {
+    validateRequiredDescription(body.description);
+}
+
+function validateRequiredDescription(description) {
+    if (!description) {
+        throw new Error("A descrição é obrigatória!");
     }
 }
--- src/controller/type-incomes/createIncomeType.js
+++ src/controller/type-incomes/createIncomeType.js
@@ -2,8 +2,19 @@ import { newCreateIncomeType } from "../../service/incomeTypeService.js";
 
 export async function create_income_type_controller(req, res) {
     try {
+        await validates(req.body);
         await newCreateIncomeType(req, res);
     } catch (error) {
         res.status(500).json({ message: "Erro interno do servidor" });
+    }
+}
+
+async function validates(body) {
+    validateRequiredDescription(body.description);
+}
+
+function validateRequiredDescription(description) {
+    if (!description) {
+        throw new Error("A descrição é obrigatória!");
     }
 }
