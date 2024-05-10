cd60dadd1ed785602fd949f2226ae69121d3a79e
GuilberthT <guilberthw@gmail.com>
2024-05-09 20:03:17 -0400

    adc middleware

--- src/router/user.js
+++ src/router/user.js
@@ -2,11 +2,13 @@ import express from 'express';
 import { registerUser } from '../controller/user/registerUser.js';
 import { loginUser } from '../controller/user/loginUser.js';
 import { getUser } from '../controller/user/getUser.js';
+import validateId from '../middleware/validateId.js';
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.get("/:id", getUser);
+router.get("/:id", validateId, getUser);
 
 export default router;
+
